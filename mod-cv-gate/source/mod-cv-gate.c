e03d197dffae383fb8f09989453f82fbe71318d4
BramGiesen <giesenbram@gmail.com>
2019-12-18 21:52:38 +0100

    Added CV gate plugin

---
+++ mod-cv-gate/source/mod-cv-gate.c
@@ -0,0 +1,129 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lv2/lv2plug.in/ns/lv2core/lv2.h"
+
+#define PLUGIN_URI "http://moddevices.com/plugins/mod-devel/mod-cv-gate"
+
+
+typedef enum {
+    L_GATE   = 0,
+    L_INPUT  = 1,
+    L_OUTPUT = 2,
+    L_ENABLE = 3
+} PortIndex;
+
+
+typedef struct {
+    float* gate;
+    float* input;
+    float* output;
+    float* plugin_enabled;
+} Gate;
+
+
+static LV2_Handle
+instantiate(const LV2_Descriptor*     descriptor,
+        double                    rate,
+        const char*               bundle_path,
+        const LV2_Feature* const* features)
+{
+    Gate* self = (Gate*)malloc(sizeof(Gate));
+
+
+    return (LV2_Handle)self;
+}
+
+
+static void
+connect_port(LV2_Handle instance,
+        uint32_t   port,
+        void*      data)
+{
+    Gate* self = (Gate*)instance;
+
+    switch ((PortIndex)port) {
+        case L_GATE:
+            self->gate = (float*)data;
+            break;
+        case L_INPUT:
+            self->input = (float*)data;
+            break;
+        case L_OUTPUT:
+            self->output = (float*)data;
+            break;
+        case L_ENABLE:
+            self->plugin_enabled = (float*)data;
+            break;
+    }
+}
+
+
+static void
+activate(LV2_Handle instance)
+{
+}
+
+
+static void
+run(LV2_Handle instance, uint32_t n_samples)
+{
+    Gate* self = (Gate*) instance;
+
+    for ( uint32_t i = 0; i < n_samples; i++)
+    {
+        if ((int)*self->plugin_enabled == 1) {
+            if (*self->gate >= 1.0) {
+                self->output[i] = self->input[i];
+            } else {
+                self->output[i] = 0.0;
+            }
+        } else {
+            self->output[i] = 0.0;
+        }
+    }
+}
+
+
+static void
+deactivate(LV2_Handle instance)
+{
+}
+
+
+static void
+cleanup(LV2_Handle instance)
+{
+    free(instance);
+}
+
+
+static const void*
+extension_data(const char* uri)
+{
+    return NULL;
+}
+
+
+static const LV2_Descriptor descriptor = {
+    PLUGIN_URI,
+    instantiate,
+    connect_port,
+    activate,
+    run,
+    deactivate,
+    cleanup,
+    extension_data
+};
+
+
+LV2_SYMBOL_EXPORT
+    const LV2_Descriptor*
+lv2_descriptor(uint32_t index)
+{
+    switch (index) {
+        case 0:  return &descriptor;
+        default: return NULL;
+    }
+}
